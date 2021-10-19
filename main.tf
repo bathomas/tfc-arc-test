2d6cf6cb0f7c4b531a10456fc9d885a423c063dc
Ben Thomas <b.a.thomas@ucl.ac.uk>
2021-10-19 15:29:35 +0100

    Use RHEL8 (#1)
    * Use RHEL8
    
    * Add MySQL 5.7 DB

--- main.tf
+++ main.tf
@@ -2,27 +2,39 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "rhel8" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["aws-rhel8-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
-  owners = ["099720109477"] # Canonical
+  
+  owners = ["239608013568"]
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
+resource "aws_instance" "rhel8" {
+  ami           = data.aws_ami.rhel8.id
   instance_type = var.instance_type
 
   tags = {
     Name = var.instance_name
   }
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
 }
